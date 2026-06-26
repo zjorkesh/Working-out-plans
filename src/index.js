2d5bae21efc2c679fe19cdb0627ada07706e2847
Zeinab <z.jorkesh1989@gmail.com>
2026-06-27 03:18:08 +0800

    Added basic form handling and Typewriting Plug in

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function generatePlan(event){
+    event.preventDefault();
+
+    let planElement = document.querySelector("#fitness-plan");
+    //planElement.innerHTML = "There is some functional practices";
+    new Typewriter(planElement, {
+  strings: ['There is some functional practices'],
+  autoStart: true,
+  delay:1,
+  cursor:"",
+});
+
+    
+}
+
+let planFormElement = document.querySelector("#plan-generator-form");
+planFormElement.addEventListener("submit", generatePlan);
