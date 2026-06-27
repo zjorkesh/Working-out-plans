feda8394ce924b75b06ba6dff1c410289cf901a1
Zeinab <z.jorkesh1989@gmail.com>
2026-06-27 18:50:50 +0800

    Improved the user Experience

--- src/index.js
+++ src/index.js
@@ -24,12 +24,14 @@ function generatePlan(event){
 
     let instructionElement = document.querySelector("#user-instructions");
     let planElement = document.querySelector("#fitness-plan");
-    planElement.innerHTML = "Generating your workout plan...";
-   
     let apiKey = "8f8ba35f23t75fbc75d7do5424f8040b";
     let prompt = `Create a weekly workout plan for ${instructionElement.value}`;
     let context = `You are a certified fitness coach. Return ONLY raw HTML. Do not use markdown. Generate a COMPLETE workout plan for all 7 days: 1, 2, 3, 4, 5, 6, and 7. write whole days'plan. Keep the response concise. Each day must have exactly 3 exercises. Use only <strong>, <br>, <ul>, and <li> tags. `;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    
+    planElement.classList.remove("hidden");
+    planElement.innerHTML = `Generating your workout plan for ${instructionElement.value}`;
+   
 
     axios.get(apiUrl).then(displayplan);
   
